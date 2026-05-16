9cb2a25cb40a4ec9bba9bbe61e69f2502a9a5a14
Yohan1220 <yjimenezh@unal.edu.co>
2026-05-15 20:52:42 -0500

    Arquitectura del proyecto

---
+++ NoPainNoMain/src/main/java/com/adanext/NoPainNoMain/NoPainNoMainApplication.java
@@ -0,0 +1,13 @@
+package com.adanext.NoPainNoMain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoPainNoMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoPainNoMainApplication.class, args);
+	}
+
+}
---
+++ NoPainNoMain/src/test/java/com/adanext/NoPainNoMain/NoPainNoMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adanext.NoPainNoMain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoPainNoMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
